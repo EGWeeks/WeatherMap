78664932c6dc05856b181953510accdaaada3131
Ethan Weeks <ethanweeks@ymail.com>
2016-06-03 21:52:17 -0600

    updated app with marker

--- js/app.js
+++ js/app.js
@@ -5,33 +5,88 @@ $(document).ready(function() {
 	var self = this;
 	self.user = {}
 
+
+
 	// get User Location
 	var getUserLocation = function() {
-	  navigator.geolocation.getCurrentPosition(function(position) {
-	  	self.user.lat = position.coords.latitude;
-	    self.user.lng = position.coords.longitude;
-
-	    reverseGeocode(self.user.lat, self.user.lng);
-	    getWeather(self.user.lat, self.user.lng);
-	  });
+		try {
+		  navigator.geolocation.getCurrentPosition(function(position) {
+		  	self.user.lat = position.coords.latitude;
+		    self.user.lng = position.coords.longitude;
+		    initMap(self.user.lat, self.user.lng);
+		    reverseGeocode(self.user.lat, self.user.lng);
+		    getWeather(self.user.lat, self.user.lng);
+		  });
+		} catch(e) {
+			alert('Ehhh... Looks like there is a problem getting your current location. Here is the Error: ' + e);
+		}
 	};
+
+
+
+	//inital a new map
+	var initMap = function(lat, lng) {
+
+	  	var mapOptions = {
+        zoom: 10,
+        center: new google.maps.LatLng(lat,lng),
+        mapTypeId: google.maps.MapTypeId.TERRAIN,
+        zoomControl: true,
+        zoomControlOptions: {
+			  	position: google.maps.ControlPosition.LEFT_CENTER
+			  },
+        mapTypeControl: false,
+			  scaleControl: false,
+			  streetViewControl: false,
+			  rotateControl: false
+    	};
+
+		self.map = new google.maps.Map($('#map')[0], mapOptions);
+
+		initMarker();
+	}
+
+
+
+	//initial a new marker
+	var initMarker = function() {
+
+		//MUST USE user obj lat and lng and not maps for modularity	
+		var coords = new google.maps.LatLng(self.user.lat, self.user.lng);
+
+		self.marker = new google.maps.Marker({
+    			position: coords,
+    			// icon: icon,
+    			animation: google.maps.Animation.DROP,
+    			draggable: true
+
+  	});
+
+		self.marker.setMap(self.map); 
+	}
+
+
 
 	//Human readable address
 	var reverseGeocode = function(lat, lng) {
 		var googleKey = 'AIzaSyAWKl-KPsCIij9Y3Ui9ounu42liHkm_egw';
 		$.get('https://maps.googleapis.com/maps/api/geocode/json?latlng='+lat+','+lng+'&key='+googleKey, function(location) {
-			console.log(location);
+
+			self.user.formatAdd = location.results[3].formatted_address;
 		});
 	};
 
+
+
 	//Weather data
 	var getWeather = function(lat, lng) {	
-		console.log(lat+ ' '+ lng);
-		var weatherKey = '35f88f2946668df8785d29c91312c21c';
-		$.get('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+'&appid='+weatherKey , function(weather){
-			// console.log(weather);
-		});
+		// var weatherKey = '35f88f2946668df8785d29c91312c21c';
+		// $.get('http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+'&appid='+weatherKey , function(weather){
+		// 	// console.log(weather);
+		// });
 	};
+
+
 
 	//Check if app has access to user location
 	var checkLocation = (function(){
